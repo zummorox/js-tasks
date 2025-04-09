028b639ae6295fc0643a2579320283f9abd12ef0
zummorox <200913vassabi@gmail.com>
2025-04-09 18:42:23 +0300

    as

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-alert("я не хочу я нехочун");
+// const x = prompt ("Введите:");
+// if(typeof(x) == Number){
+//     alert("Вы ввели число");
+// }else{
+//     alert("Это не число!!!");
+// }
+
+
+
+let x = prompt("Введите:");
+let y = Number(x);
+if(y != Number){
+    alert("Ошибка!");
+}
